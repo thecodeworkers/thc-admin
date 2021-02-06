9372c79af9e77d91f33ba6117fb0936207905429
Victor Caceres <caceres.vc95@gmail.com>
2021-02-06 01:07:17 -0400

    create login view

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,60 +1,46 @@
 import { Layout } from '../components';
+import styles from '../styles/Login.module.scss';
 
 const Login = () => (
-  <div style={{ backgroundColor: 'red', minHeight: '100%', width: '100%' }}>
+  <Layout>
+    <div className={styles.container}>
+      <div className={`login-box ${styles.upLoginBox}`}>
+        <div className="login-logo">
+          <a href="../../index2.html"><b>Admin</b>LTE</a>
+        </div>
+        <div className="login-box-body">
+          <p className="login-box-msg">Sign in to start your session</p>
 
-  </div>
-  // <Layout>
-  //   <body className="hold-transition login-page">
-  //     <div className="login-box">
-  //       <div className="login-logo">
-  //         <a href="../../index2.html"><b>Admin</b>LTE</a>
-  //       </div>
-  //       <div className="login-box-body">
-  //         <p className="login-box-msg">Sign in to start your session</p>
+          <form action="../../index2.html" method="post">
+            <div className="form-group has-feedback">
+              <input type="email" className="form-control" placeholder="Email" />
+              <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div className="form-group has-feedback">
+              <input type="password" className="form-control" placeholder="Password" />
+              <span className="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div className="row">
+              <div className="col-xs-8">
+                <div className="checkbox icheck">
+                  <label>
+                    <input type="checkbox" /> Remember Me
+                  </label>
+                </div>
+              </div>
 
-  //         <form action="../../index2.html" method="post">
-  //           <div className="form-group has-feedback">
-  //             <input type="email" className="form-control" placeholder="Email" />
-  //             <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
-  //           </div>
-  //           <div className="form-group has-feedback">
-  //             <input type="password" className="form-control" placeholder="Password" />
-  //             <span className="glyphicon glyphicon-lock form-control-feedback"></span>
-  //           </div>
-  //           <div className="row">
-  //             <div className="col-xs-8">
-  //               <div className="checkbox icheck">
-  //                 <label>
-  //                   <input type="checkbox" /> Remember Me
-  //                 </label>
-  //               </div>
-  //             </div>
+              <div className="col-xs-4">
+                <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
+              </div>
 
-  //             <div className="col-xs-4">
-  //               <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
-  //             </div>
+            </div>
+          </form>
 
-  //           </div>
-  //         </form>
-
-  //         <div className="social-auth-links text-center">
-  //           <p>- OR -</p>
-  //           <a href="#" className="btn btn-block btn-social btn-facebook btn-flat"><i className="fa fa-facebook"></i> Sign in using
-  //             Facebook</a>
-  //           <a href="#" className="btn btn-block btn-social btn-google btn-flat"><i className="fa fa-google-plus"></i> Sign in using
-  //             Google+</a>
-  //         </div>
-          
-
-  //         <a href="#">I forgot my password</a><br />
-  //         <a href="register.html" className="text-center">Register a new membership</a>
-
-  //       </div>
-        
-  //     </div>
-  //   </body>
-  // </Layout>
+          <a href="#">I forgot my password</a><br />
+        </div>
+      </div>
+    </div>
+  </Layout>
 )
 
 export default Login
