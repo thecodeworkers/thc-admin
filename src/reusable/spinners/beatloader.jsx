e69855a993ef328f823c9861d3cc75611247387b
Armando Zubillaga <ajzpiv97@gmail.com>
2021-03-23 15:36:07 -0400

    changed name

--- src/reusable/spinners/beatloader.jsx
+++ src/reusable/spinners/beatloader.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { css } from "@emotion/react";
 import { BeatLoader } from "react-spinners";
 
-export default () => {
+const Loading = (props) => {
   const override = css`
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
   `;
-  return <BeatLoader size={30} css={override} color={"red"} />;
+  return <BeatLoader size={30} css={override} color={props.color} />;
 };
+
+export default Loading;
