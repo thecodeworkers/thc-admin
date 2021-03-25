c40f46c648f9daaeb9e477a5659aafd0d3c192bc
Armando Zubillaga <ajzpiv97@gmail.com>
2021-03-25 19:42:02 -0400

    init

---
+++ src/views/pages/login/LoginForm.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { Formik, Form, Field } from "formik";
+import { CButton, CCardBody, CInputGroupPrepend } from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import {
+  FormInputField,
+  ErrorMessageSpace,
+} from "../../../reusable/form/input";
+
+const LoginForm = (props) => {
+  return null;
+};
