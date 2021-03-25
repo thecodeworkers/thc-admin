fed2aa2a2ae167c063bb5dc8b10c61727e0ea0d1
Armando Zubillaga <ajzpiv97@gmail.com>
2021-03-25 19:55:17 -0400

    Added formik

--- src/views/pages/login/LoginForm.jsx
+++ src/views/pages/login/LoginForm.jsx
@@ -1,12 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { Formik, Form, Field } from "formik";
-import { CButton, CCardBody, CInputGroupPrepend } from "@coreui/react";
-import CIcon from "@coreui/icons-react";
+import { CButton, CCardBody, CRow, CCol } from "@coreui/react";
 import {
   FormInputField,
   ErrorMessageSpace,
 } from "../../../reusable/form/input";
 
 const LoginForm = (props) => {
-  return null;
+  return (
+    <Formik
+      initialValues={props.formValues}
+      onSubmit={(values, { resetForm }) => {
+        props.postParams(values, resetForm);
+      }}
+      validationSchema={props.registrationSchema}
+    >
+      {({ handleChange }) => (
+        <CCardBody className="p-3">
+          <Form onChange={handleChange}>
+            <FormInputField
+              icon="cil-envelope-closed"
+              name="email"
+              inputType="text"
+              placeholder="Correo Electronico"
+              popover={null}
+            />
+            <ErrorMessageSpace name="email" />
+            <CRow>
+              <CCol xs="6">
+                <CButton color="primary" className="px-4">
+                  Login
+                </CButton>
+              </CCol>
+              <CCol xs="6" className="text-right">
+                <CButton color="link" className="px-0">
+                  Forgot password?
+                </CButton>
+              </CCol>
+            </CRow>
+          </Form>
+        </CCardBody>
+      )}
+    </Formik>
+  );
 };
+
+export default LoginForm;
--- src/views/pages/login/index.jsx
+++ src/views/pages/login/index.jsx
@@ -15,6 +15,7 @@ import {
   CRow,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
+import LoginForm from "./LoginForm";
 
 const Login = () => {
   return (
@@ -23,49 +24,12 @@ const Login = () => {
         <CRow className="justify-content-center">
           <CCol md="8">
             <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-muted">Sign In to your account</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupPrepend>
-                        <CInputGroupText>
-                          <CIcon name="cil-user" />
-                        </CInputGroupText>
-                      </CInputGroupPrepend>
-                      <CInput
-                        type="text"
-                        placeholder="Username"
-                        autoComplete="username"
-                      />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupPrepend>
-                        <CInputGroupText>
-                          <CIcon name="cil-lock-locked" />
-                        </CInputGroupText>
-                      </CInputGroupPrepend>
-                      <CInput
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="current-password"
-                      />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs="6">
-                        <CButton color="primary" className="px-4">
-                          Login
-                        </CButton>
-                      </CCol>
-                      <CCol xs="6" className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
-                        </CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                </CCardBody>
+              <CCard className="p-2">
+                <div style={{ marginLeft: "10px", marginBottom: "10px" }}>
+                  <h1>Acceso</h1>
+                  <p className="text-muted">Ingrese a su cuenta</p>
+                </div>
+                <LoginForm />
               </CCard>
               <CCard
                 className="text-white bg-primary py-5 d-md-down-none"
