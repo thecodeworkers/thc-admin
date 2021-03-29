8e0a1c9b6eba63fa7d90024c92f9c120742b73df
Armando Zubillaga <32502491+ajzpiv97@users.noreply.github.com>
2021-03-28 23:55:02 -0400

    Update index.jsx

--- src/views/pages/register/index.jsx
+++ src/views/pages/register/index.jsx
@@ -77,8 +77,8 @@ const Register = (props) => {
   const formValues = {
     email: "",
     roleType: "",
-    password: "Cimabel1$",
-    confirmPassword: "Cimabel1$",
+    password: "",
+    confirmPassword: "",
   };
 
   const registrationSchema = Yup.object().shape({
